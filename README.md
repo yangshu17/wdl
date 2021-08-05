ae85e7a263346721f56d7cf21aa7c3937060d72a
yangshu <yangshu@huohua.com>
2021-08-05 16:46:21 +0800

    删除多余代码

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # 云开发 quickstart
 
+本地允许终端：mac：sh ./uploadCloudFunction.sh || ./uploadCloudFunction.bat
+
 这是云开发的快速启动指引，其中演示了如何上手使用云开发的三大基础能力：
 
 - 数据库：一个既可在小程序前端操作，也能在云函数中读写的 JSON 文档型数据库
@@ -7,6 +9,4 @@
 - 云函数：在云端运行的代码，微信私有协议天然鉴权，开发者只需编写业务逻辑代码
 
 ## 参考文档
-
-- [云开发文档](https://developers.weixin.qq.com/miniprogram/dev/wxcloud/basis/getting-started.html)
 
--- cloudfunctions/quickstartFunctions/index.js
+++ cloudfunctions/quickstartFunctions/index.js
@@ -1,10 +1,7 @@
 const getOpenId = require('./getOpenId/index')
 const getMiniProgramCode = require('./getMiniProgramCode/index')
-const createCollection = require('./createCollection/index')
 const selectRecord = require('./selectRecord/index')
 const updateRecord = require('./updateRecord/index')
-const sumRecord = require('./sumRecord/index')
-
 const createData = require('./createData/index')
 const addGood = require('./addGood/index')
 
@@ -12,21 +9,24 @@ const addGood = require('./addGood/index')
 // 云函数入口函数
 exports.main = async (event, context) => {
   switch (event.type) {
+    // 获取openid
     case 'getOpenId':
       return await getOpenId.main(event, context)
+    // 上传图片？
     case 'getMiniProgramCode':
       return await getMiniProgramCode.main(event, context)
-    case 'createCollection':
-      return await createCollection.main(event, context)
+      
+    // 查
     case 'selectRecord':
       return await selectRecord.main(event, context)
+    // 改
     case 'updateRecord':
       return await updateRecord.main(event, context)
-    case 'sumRecord':
-      return await sumRecord.main(event, context)
 
+    // 创建
     case 'createData':
       return await createData.main(event, context)
+    // 添加
     case 'addGood':
       return await addGood.main(event, context)
   }
